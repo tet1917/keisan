6e5821bde676b7db3d19268309f9e62f8a631dfc
tet1917 <tetsu0224.soccer@icloud.com>
2023-02-23 06:47:30 +0900

    first commit

---
+++ src/test/java/com/example/demo/KeisanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeisanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
